48757a57f9a8ecbca26c30075e8fc79af58780b5
pitimon <ip@en.rmutt.ac.th>
2021-05-05 10:51:09 +0700

    localIP info update

--- timeEst.sh
+++ timeEst.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "############################"
 date
+curl ipinfo.io
 dig $1 +short
 curl -o /dev/null -s -w 'Establish Connection: %{time_connect}s\nTTFB: %{time_starttransfer}s\nTotal: %{time_total}s\n'  https://$1
 echo "#############################"
